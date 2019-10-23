33df82f1b53f9084ef1a20a1552db451de23eaaf
David Bragmark <david.bragmark@valtech.com>
2019-10-23 16:51:40 +0200

    Part 3.

--- new_task.py
+++ new_task.py
@@ -1,9 +1,23 @@
+import pika
 import sys
 
+credentials = pika.PlainCredentials('guest', 'guest')
 
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=credentials))
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+message = ' '.join(sys.argv[1:]) or 'Hello world!'
+
 channel.basic_publish(exchange='',
-                      routing_key='q.example',
-                      body=message)
-print(" [x] Sent %r" % message)
+                        routing_key='task_queue',
+                        body=message,
+                        properties=pika.BasicProperties(
+                            delivery_mode=2
+                        ))
+
+print(' [x] sent %r' % message)
+
+connection.close()
